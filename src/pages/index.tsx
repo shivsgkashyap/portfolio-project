14362fcfd3e82f472969eb6ac8a0e4547f30dca4
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-14 13:15:53 +0000

    Added global styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen">
       <Head>
         <title>Shiv Kashyap</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -14,6 +15,9 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About */}
 
@@ -24,6 +28,6 @@ export default function Home() {
       {/* Projects */}
 
       {/* Contact Me */}
-    </>
+    </div>
   );
 }
