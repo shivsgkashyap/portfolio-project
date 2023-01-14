05e6762b4a55dd1cc0e6ee488d42cf0a1e2ad0d5
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-14 12:28:34 +0000

    HTML and basic styling for navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
@@ -9,7 +10,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-red-500">Hello World</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
     </>
   );
 }
