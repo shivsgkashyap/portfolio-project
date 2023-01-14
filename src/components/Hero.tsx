c5db59d7068d88a9858f88245c36daae4b4f85cd
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-14 14:44:51 +0000

    Added nav links to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -7,7 +8,7 @@ type Props = {};
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
     words: [
-      "Hi there, the name's Shiv Kashyap",
+      "Hi there, the name's Shiv",
       "Guy-who-loves-Coffee.tsx",
       "<ButLovesToCodeMore />",
     ],
@@ -18,10 +19,33 @@ const Hero = (props: Props) => {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
+      <h1 className="relative text-7xl lg:text-8xl mx-auto tracking-wider uppercase text-[#F7AB0A]">
+        Shiv Kashyap
       </h1>
+      <div className="mt-6">
+        <h3 className="text-sm uppercase text-gray-500 pb-4 tracking-[15px]">
+          Software Engineer
+        </h3>
+        <h2 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h2>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
