8cde4f31abf15a38c5984194ab23a9c8d5a1da57
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-14 14:07:27 +0000

    Added animation for background circles in hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Shiv Kashyap</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -15,7 +15,7 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
 
