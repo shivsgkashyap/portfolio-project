d40d7787d0ab6ebd989a2fad3955b6033865ce59
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-15 13:41:57 +0000

    Installed and set up schema for sanity io

---
+++ sanity/schemas/skill.ts
@@ -0,0 +1,28 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'skill',
+  title: 'Skill',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'skillName',
+      title: 'SkillName',
+      type: 'string',
+      description: 'Name of the Skill',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
