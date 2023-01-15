87564ab305bc5ed4dc9e263ed74615d48546a282
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-15 12:02:55 +0000

    Coded and styled contact, added react hook forms

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type Props = {};
 
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
 const ContactMe = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -14,6 +29,39 @@ const ContactMe = (props: Props) => {
           I have got just what you need.{" "}
           <span className="underline decoration-[#F7AB0A]/50">Let's talk.</span>
         </h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+44 7772 451177</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">ssgkashyap@outlook.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">123 Developer Lane</p>
+          </div>
+        </div>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input placeholder="Name" className="contactInput" type="text" />
+            <input placeholder="Email" className="contactInput" type="email" />
+          </div>
+
+          <input placeholder="Subject" className="contactInput" type="text" />
+
+          <textarea placeholder="Message" className="contactInput"></textarea>
+
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
