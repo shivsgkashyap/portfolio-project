13d5a376e5d7c393fbb0eabb34e2715850403dbf
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-15 13:41:36 +0000

    Installed and set up schema for sanity io

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,6 @@
+import blockContent from './social'
+import category from './project'
+import post from './pageInfo'
+import author from './skill'
+
+export const schemaTypes = [post, author, category, blockContent]
